563a72ac5bcca39e01024ce366a6706a99b44c3d
Nikita Albert <nikitaalbert@gmail.com>
2025-05-13 09:19:44 -0500

    Initial commit of run script, training_script.py, and diabetes data.

---
+++ ScriptRunConfig.py
@@ -0,0 +1,20 @@
+from azureml.core import Experiment, ScriptRunConfig, Environment
+from azureml.core.conda_dependencies import CondaDependencies
+
+# Create a Python environment for the experiment
+sklearn_env = Environment("sklearn-env")
+
+# Ensure the required packages are installed
+packages = CondaDependencies.create(conda_packages=['scikit-learn','pip'],
+                                    pip_packages=['azureml-defaults'])
+sklearn_env.python.conda_dependencies = packages
+
+# Create a script config
+script_config = ScriptRunConfig(source_directory='training_folder',
+                                script='training_script.py',
+                                environment=sklearn_env) 
+
+# Submit the experiment
+experiment = Experiment(workspace=ws, name='training-experiment')
+run = experiment.submit(config=script_config)
+run.wait_for_completion()
---
+++ training_folder/training_script.py
@@ -0,0 +1,35 @@
+from azureml.core import Run
+import pandas as pd
+import numpy as np
+import os
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Get the experiment run context
+run = Run.get_context()
+
+# Prepare the dataset
+diabetes = pd.read_csv('data.csv')
+
+# Nikita Albert's Note: Patient ID should not be a part of the training loop, 
+# since we do not want an ID number to influence a model's decisioning
+# as to whether the given entry has diabetes or not.
+# I am including it because the exercise did not call for its removal.
+X, y = diabetes[['PatientID', 'Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, diabetes['Diabetic'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+# Train a logistic regression model
+reg = 0.1
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+# calculate accuracy
+y_hat = model.predict(X_test)
+acc = np.average(y_hat == y_test)
+run.log('Accuracy', np.float(acc))
+
+# Save the trained model
+os.makedirs('outputs', exist_ok=True)
+joblib.dump(value=model, filename='outputs/model.pkl')
+
+run.complete()
